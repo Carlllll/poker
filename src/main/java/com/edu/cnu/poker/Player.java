eea0f71ba7e33fafe9ab32490676054342387785
dhksehdtj <dhksehdtj@naver.com>
2017-04-27 21:17:30 +0900

    DrawAgain - who want's more game

--- src/main/java/com/edu/cnu/poker/Player.java
+++ src/main/java/com/edu/cnu/poker/Player.java
@@ -6,7 +6,7 @@ import java.io.*;
 // round 변수를 가지고 있어서 라운드마다 점수를 집계한 뒤,
 // 승자를 결정한다
 // round를 주고 받으려면 플레이어가 turn을 주고 받으면서 각자의 게임을 진행해야한다 (taketurn 함수)
-//
+// DrawAgain 함수를 이용하여 게임을 계속 이어가고 싶은경우 true, 그만 하겠다 하면 falsea로 리턴하게 해준다
 //
 //
 public class Player {
@@ -17,9 +17,27 @@ public class Player {
 	private int Sumofscore = 0;
 	private int RoundCount = 0;
 
-	public Player(Card card){
+	BufferedReader buf = new BufferedReader();
+
+	public Player(Card card){ // 하나이상의 플레이어를 정의하기 위한 player 클래스
 		this.card = card;
 		this.score = 0;
+    }
+
+	private boolean DrawAgain(){
+		System.out.print("Do you want more game? (y/n): ");
+		try {
+			String input = buf.readLine();
+			if(input.equals("n"))
+			    return false;
+			else if(input.equals("y"))
+			    return true;
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+
 	}
 
 	public int Score(){
