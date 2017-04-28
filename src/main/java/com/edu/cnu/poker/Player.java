867a97a8d28cf03d18b8f70afe80c56d567fe5a1
dhksehdtj <dhksehdtj@naver.com>
2017-04-28 19:31:28 +0900

    Method should not have parameter, NullPointException problem, cannot solve

--- src/main/java/com/edu/cnu/poker/Player.java
+++ src/main/java/com/edu/cnu/poker/Player.java
@@ -8,7 +8,7 @@ import static java.lang.System.*;
 // round 변수를 가지고 있어서 라운드마다 점수를 집계한 뒤,
 // 승자를 결정한다
 // round를 주고 받으려면 플레이어가 turn을 주고 받으면서 각자의 게임을 진행해야한다 (taketurn 함수)
-// DrawAgain 함수를 이용하여 게임을 계속 이어가고 싶은경우 true, 그만 하겠다 하면 falsea로 리턴하게 해준다
+// DrawAgain 함수를 이용하여 게임을 계속 이어가고 싶은경우 true, 그만 하겠다 하면 false로 리턴하게 해준다
 //
 //
 public class Player {
@@ -48,12 +48,12 @@ public class Player {
 		return this.Score;
 	}
 
-	public void takeTurn(int Round){
+	public void takeTurn(){
 		do{
 			if(RoundCount == 0){
 				this.firstScore = score;
 				this.Sumofscore = this.firstScore;
-				out.print("Your first score is " + this.Sumofscore + " ");
+				System.out.println("Your first score is " + this.Sumofscore + " ");
 			}
 
 			RoundCount++;
--- src/test/java/com/edu/cnu/poker/PlayerTest.java
+++ src/test/java/com/edu/cnu/poker/PlayerTest.java
@@ -14,31 +14,27 @@ public class PlayerTest {
     private Player player1;
     private Player player2;
 
-
     @Test
-    public void playGame(){
-        for(int i = 1; i <= 3; i++){
-            System.out.println("Round " + i + ":");
+    public void playGame() {
+            System.out.println("Round " + 1 + ":");
             playRound();
+            if (player1.Score()> player2.Score()) {
+                System.out.println("Player 1 wins.");
+            } else if (player1.Score() < player2.Score()) {
+                System.out.println("Player 2 wins.");
+            } else {
+                System.out.println("Draw.");
+            }
         }
-        if(player1.Score() > player2.Score()){
-            System.out.println("Player 1 wins.");
-        }
-        else if(player1.Score() < player2.Score()){
-            System.out.println("Player 2 wins.");
-        }
-        else{
-            System.out.println("Draw.");
-        }
-    }
+
     @Test
     public void playRound(){
         System.out.println("Player 1:");
-        player1.takeTurn(3);
+       // player1.takeTurn();
         System.out.println("Player 2:");
-        player2.takeTurn(3);
-        System.out.println("Player 1 score : " + player1.Score());
-        System.out.println("Player 2 score : " + player2.Score());
+  //      player2.takeTurn();
+//        System.out.println("Player 1 score : " + player1.Score());
+  //      System.out.println("Player 2 score : " + player2.Score());
     }
 
 }
